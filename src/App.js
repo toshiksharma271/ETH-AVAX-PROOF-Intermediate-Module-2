dfc213fd19206b5db1d4cc2c3e2034e478d4af4c
Toshik Sharma <toshiksharma45@gmail.com>
2024-06-21 08:56:13 +0000

    MetaMask Connected

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import WalletCard from './WalletCard';
 
+
 function App() {
 
   return (
-    <div className="App">
+    <div className="App" >
     <WalletCard/>
 
     </div>
